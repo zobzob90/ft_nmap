a19a1c7fd8baed6a2f180d12f15d53a4db91bfb3
Eric <eric.trignau@gmail.com>
2026-03-06 16:12:32 +0100

    base archi

---
+++ inc/nmap.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nmap.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eric <eric@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/06 15:43:11 by eric              #+#    #+#             */
+/*   Updated: 2026/03/06 16:12:14 by eric             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NMAP_H
+#define NMAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <netdb.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/types.h>
+
+#include <arpa/inet.h>
+#include <pthread.h>
+
+typedef struct s_nmap
+{
+	
+} t_nmap;
+
+#endif
